8dbcfbcb860a752de8f5f07bf9009b4005d3adf3
giovannimegliola <giovanni.megliola@hotmail.it>
2024-01-16 17:08:31 +0100

    modified projects images

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -2,9 +2,12 @@
 @section('content')
     <section class="container">
         <h1>{{$project->title}}</h1>
-        <p>{{$project->body}}</p>
+        <div class="badge bg-secondary">
+            {{$project->category ? $project->category->name : 'Uncategorized'}}
+        </div>
+        <p class="mt-4">{{$project->body}}</p>
         <img src="{{ asset('storage/' . $project->image)}}" alt="{{$project->title}}" class="w-50">
-        <div>{{$project->category ? $project->category->name : 'Uncategorized'}}</div>
+
 
         <div class="my-3">
             <a href="{{route('admin.projects.edit', $project->slug)}}" class="btn btn-primary me-2"><i class="fa-solid fa-pen"></i> Edit Project</i></a>
